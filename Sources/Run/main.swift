866dad1eddc4e6507b1c144756d9e5dbe5d88b14
gargrashmi1 <gargrashmi1@gmail.com>
2019-09-06 11:45:11 +0530

    Update main.swift

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -24,21 +24,6 @@ import VaporAPNS
 /// if no command is given, it will default to "serve"
 
 
-setenv("DATABASE_URL", "postgres://hygziviienbche:90c60426363db158e435caf81ef5748070a8654874cd18150b1b4f9aeca4c7b2@ec2-107-21-233-72.compute-1.amazonaws.com/d8sug7gqkae4om", 1)
-
-setenv("SENDBIRD_TOKEN", "582dd7133f22f3f548566b666295ac9b72c1b069", 1)
-
-
-setenv("TWILIO_FROM", "+12133220623", 1)
-setenv("TWILIO_SID", "AC40e82047b23537e9deeda88386a29185", 1)
-setenv("TWILIO_TOKEN", "926aa405e467d695138f670f5e278ee9", 1)
-
-
-//old working
-//setenv("TWILIO_FROM", "+12133220623", 1)
-//setenv("TWILIO_SID", "AC40e82047b23537e9deeda88386a29185", 1)
-//setenv("TWILIO_TOKEN", "ed9278dafdd7bbfa963b3071351260c2", 1)
-
 
 
 let config = try Config()
